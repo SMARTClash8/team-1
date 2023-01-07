4a094a78a9ee4977400f629bad4956d96e136d25
Misha Kononenko <misha.kononenko2003@gmail.com>
2023-01-07 21:59:59 +0100

    convenient interface

---
+++ main/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .forms import UserForm
+from .models import Links
+from .test import save_photo
+def index(request):
+	if request.method == 'POST':
+		form = UserForm(request.POST, request.FILES)
+ 
+		if form.is_valid():
+			form.save()
+			return redirect('success')
+	else:
+		form = UserForm()
+	return render(request, 'html/index.html', {'form': form})
+
+def success(request):
+	return HttpResponse('successfully uploaded')
+
+	# news = Links.objects.all()
+	# if request.method == "POST":
+	# 	form = UserForm(request.POST)
+	# 	if form.is_valid():
+	# 		form.save()
+	# print(f"\n\n {news[13]} \n\n")
+	# save_photo(news[21])
+
+
+	# form = UserForm()
+	# data = {
+	# 	'form': form,
+	# 	'news': news
+	# }
+	# return render(request, 'html/index.html', data)
--- team_1/main/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Links(models.Model):
-	link = models.CharField("Link", max_length=500)
-
-	def __str__(self):
-		return self.link

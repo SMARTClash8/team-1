b7e3db23f95e3e17fa4f67abd9477fef8f6976ff
Misha Kononenko <misha.kononenko2003@gmail.com>
2023-01-08 19:20:14 +0100

    check
    check

--- main/views.py
+++ main/views.py
@@ -5,18 +5,21 @@ from .models import Links
 from .test import save_photo
 from .detete_all_files import delete_images
 def index(request):
-	delete_images('./media/images')
+	# delete_images('./media/images')
+	new = 122
 	if request.method == 'POST':
-		# delete_images('../media/images')
 		form = UserForm(request.POST, request.FILES)
 		photo_name = str(request.FILES["link"])
-		print(photo_name)
-		if form.is_valid():
-			form.save()
-			# return redirect('success')
+		if photo_name[-4:] == 'jpeg' or photo_name[-4:] == 'jfif' or photo_name[-3:] == 'jpg':
+			if form.is_valid():
+				form.save()
+				new = 124
+				# return redirect('success')
+		else:
+			return HttpResponse('The photo format is not suitable')
 	else:
 		form = UserForm()
-	return render(request, 'html/index.html', {'form': form})
+	return render(request, 'html/index.html', {'form': form, 'new': new})
 
 def success(request):
 	return HttpResponse('successfully uploaded')

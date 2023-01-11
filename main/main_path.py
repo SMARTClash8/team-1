f13c4968f2929abe272600d526b899ec324759dc
Misha Kononenko <misha.kononenko2003@gmail.com>
2023-01-11 18:39:10 +0100

    dev

--- main/main_path.py
+++ main/main_path.py
@@ -1,2 +1,2 @@
-PATH_FOLDER_MODEL = 'C:/Users/misha/Downloads/'
+PATH_FOLDER_MODEL = 'C:/Users/misha/Desktop/python/team_1/main/model/'
 PATH_FOLDER_PROJECT = 'C:/Users/misha/Desktop/python/'
--- main/views.py
+++ main/views.py
@@ -6,12 +6,12 @@ from .test import save_photo
 from utils.module_model import model_loader, image_classify, get_photo_path
 from .main_path import PATH_FOLDER_MODEL, PATH_FOLDER_PROJECT
 from .detete_all_files import delete_images
-model_path = (PATH_FOLDER_MODEL + "model_from_Susana.h5", 
-			  PATH_FOLDER_MODEL + "weights_from_Susana.h5")
+model_path = (PATH_FOLDER_MODEL + "model_from_mentor.h5", 
+			  PATH_FOLDER_MODEL + "weights_from_mentor.h5")
 model = model_loader(model_path)
 
 def index(request):
-	result = ''
+	string = ''
 	if request.method == 'POST':
 		form = UserForm(request.POST, request.FILES)
 		delete_images('./media/images')
@@ -19,9 +19,11 @@ def index(request):
 			x = form.save()
 			full_name = PATH_FOLDER_PROJECT + "team_1/media/images/" + str(request.FILES['link'])
 			result = image_classify(full_name, 'si_rescale', model)
+			# print(round(result[0][2]*100, 2))
+			string = "This is the " + result[0][1] + " with chance " + str(round(result[0][2]*100, 2)) + "%"
 	else:
 		form = UserForm()
-	return render(request, 'html/index.html', {'form': form, 'result': result})
+	return render(request, 'html/index.html', {'form': form, 'result': string})
 
 def success(request):
 	return HttpResponse('successfully uploaded')
